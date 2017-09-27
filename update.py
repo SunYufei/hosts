07f0669bb093ce7677ef625bb2ec6af1d2541f46
SunYufei <sunyufei963@gmail.com>
2017-09-27 22:15:46 +0800

    Update 2017/9/27

--- update.py
+++ update.py
@@ -6,7 +6,7 @@ import shutil
 
 HOSTS_URL = 'https://coding.net/u/scaffrey/p/hosts/git/raw/master/hosts-files/hosts'
 
-LOCAL_PATH = r'C:\Windows\System32\drivers\etc\hosts'
+LOCAL_PATH = os.environ['windir'] + '\\System32\\drivers\\etc\\hosts'
 
 
 def main():
@@ -14,16 +14,22 @@ def main():
     # Download hosts file
     if(os.path.exists('hosts')):
         os.remove('hosts')
-    urllib.request.urlretrieve(HOSTS_URL, 'hosts')
+    try:
+        urllib.request.urlretrieve(HOSTS_URL, 'hosts')
+    except Exception:
+        print('网络连接失败')
     # Backup old hosts file
-    if(os.path.exists(LOCAL_PATH + '.bak')):
-        os.remove(LOCAL_PATH)
-    else:
-        shutil.copyfile(LOCAL_PATH, LOCAL_PATH + '.bak')
-    # Move new file to system
-    shutil.move('hosts', LOCAL_PATH)
-    # Update dns cache
-    os.system('ipconfig /flushdns')
+    try:
+        if(os.path.exists(LOCAL_PATH + '.bak')):
+            os.remove(LOCAL_PATH)
+        else:
+            shutil.copyfile(LOCAL_PATH, LOCAL_PATH + '.bak')
+        # Move new file to system
+        shutil.move('hosts', LOCAL_PATH)
+        # Update dns cache
+        os.system('ipconfig /flushdns')
+    except Exception:
+        print('请使用管理员权限运行')
 
 
 if __name__ == '__main__':
